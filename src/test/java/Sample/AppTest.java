8640b8ea74a45cd6ae804b85375377fd0b741407
antelope12 <prkp400@gmail.com>
2017-07-07 23:55:08 -0700

    initialupload

---
+++ src/test/java/Sample/AppTest.java
@@ -0,0 +1,14 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void a(){
+		System.out.println("a is pass");
+	}
+}
